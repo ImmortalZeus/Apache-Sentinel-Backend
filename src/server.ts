ff6ab9603310f25b1bd07a26414c08402fcee491
unknown <phambaduy2005@gmail.com>
2026-06-16 09:42:12 +0700

    fix: add http://127.0.0.1:5173 to CORS

--- src/server.ts
+++ src/server.ts
@@ -34,7 +34,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173'
+  ],
   credentials: true,
 })); // Allow the Vite frontend to access this API with credentials
 
