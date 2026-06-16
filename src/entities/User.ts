0d31d6d421f9d30b16abc43a3e32c7e01b4628fa
unknown <phambaduy2005@gmail.com>
2026-06-16 08:26:54 +0700

    feat: add User entity

---
+++ src/entities/User.ts
@@ -0,0 +1,17 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IUser extends Document {
+  username: string;
+  password: string; // hashed
+  role: string;
+  createdAt: Date;
+}
+
+const UserSchema = new Schema<IUser>({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, default: 'admin' },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export const User = mongoose.model<IUser>('User', UserSchema);
